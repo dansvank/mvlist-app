4f1cadddc0044326e13e2c1eaf3337a621ff6a82
dansvank <30800083-dansvank@users.noreply.replit.com>
2024-01-01 05:02:44 +0000

    testing git

--- main.py
+++ main.py
@@ -1,11 +1,28 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+
+JOBS = [{
+    'id': 1,
+    'title': 'Data Analyst',
+    'location': 'Bengaluru, india',
+    'salary': 'Rs. 10,00,000'
+}, {
+    'id': 2,
+    'title': 'Data Scientist',
+    'location': 'Delhi, india',
+    'salary': 'Rs. 15,00,000',
+}, {
+    'id': 3,
+    'title': 'Middle Ager',
+    'location': 'San Jose, Costa Rica',
+    'salary': 'CRC. 1,000,000',
+}]
 
 
 @app.route("/")
 def home():
-  return "<p>Hello World</p>"
+  return render_template("home.html", jobs=JOBS)
 
 
 if __name__ == "__main__":
